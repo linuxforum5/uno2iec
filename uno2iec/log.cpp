1d26b44966fde83a66bb3d4c3793d51047ab7c77
linuxforum5 <36374732+linuxforum5@users.noreply.github.com>
2022-12-30 11:44:02 +0100

    Array of struct with String in PROGMEM correction
    This is the valid String management, in PROGMEM array. My arduino nano reboot forever without this change. With this change works correctly.

--- uno2iec/log.cpp
+++ uno2iec/log.cpp
@@ -3,10 +3,14 @@
 
 #ifndef NO_LOGGING
 
+const char MAIN_STR[]  PROGMEM = "MAIN";
+const char IEC_STR[]   PROGMEM = "IEC";
+const char IFACE_STR[] PROGMEM = "IFACE";
+
 const struct {
 	const char abbreviated;
 	const char *string;
-} facilities[] PROGMEM = { FAC_MAIN, "MAIN", FAC_IEC, "IEC", FAC_IFACE, "IFACE" };
+} facilities[] PROGMEM = { FAC_MAIN, MAIN_STR, FAC_IEC, IEC_STR, FAC_IFACE, IFACE_STR };
 
 static const char siwe[] PROGMEM = "SIWE";
 
@@ -15,7 +19,7 @@ void registerFacilities(void)
 	char strBuf[25];
 	for(byte i = 0; i < sizeof(facilities) / sizeof(facilities[0]); ++i) {
 		sprintf_P(strBuf, (PGM_P)F("!%c"), pgm_read_byte(&facilities[i].abbreviated));
-		strcat_P(strBuf, (PGM_P)facilities[i].string);
+                strcat_P(strBuf, (PGM_P)pgm_read_word(&(facilities[i].string)) );
 		COMPORT.print(strBuf);
 		COMPORT.print('\r');
 	}
